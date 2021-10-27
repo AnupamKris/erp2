b28165ea1c73ac5c265141e67bc0593a7de80503
AnupamKris <anupamkris13262@gmail.com>
2021-10-27 15:05:12 +0530

    added new things

--- src/components/ScrollCard.vue
+++ src/components/ScrollCard.vue
@@ -1,6 +1,16 @@
 <template>
+  <button @click="previous">previous</button>
+  <button @click="next">next</button>
   <div class="cards">
-    <div className="card" v-for="card in cards" :key="card.id">
+    <div
+      class="card"
+      v-for="(card,index) in cards"
+      :key="card.id"
+      :class="{
+        previous :   card.id < currentCard ? true : false,
+        next :   card.id > currentCard ? true : false
+      }"
+    >
       {{ card.id }} -- {{ card.data }}
     </div>
   </div>
@@ -10,24 +20,61 @@
 import { ref } from "vue";
 export default {
   setup() {
-    const currentCard = ref(0);
+    const currentCard = ref(2);
+    const next = () => {
+      if (currentCard.value != 3)
+      currentCard.value++
+    }
+    const previous = () => {
+      if (currentCard.value != 1)
+      currentCard.value--
+    }
     const cards = ref([
       {
         id: 1,
         data: "card1",
       },
+      {
+        id: 2,
+        data: "card2",
+      },
+      {
+        id: 3,
+        data: "card3",
+      },
     ]);
     return {
       cards,
+      currentCard,
+      next,
+      previous
     };
   },
 };
 </script>
 
 <style lang="less" scoped>
+.cards {
+  width: 300px;
+  height: 300px;
+  position: relative;
+}
 .card {
+  transition: 0.3s;
   width: 300px;
   height: 300px;
   background: #131313;
+  border-radius: 10px;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+.previous {
+  transform: scale(0.6);
+  left: -150px;
+}
+.next {
+  transform: scale(0.6);
+  right: -150px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import LitepieDatepicker from "litepie-datepicker";
 
 createApp(App)
-  .use(LitepieDatepicker)
-  .use(store)
+ .use(store)
   .use(router)
   .mount("#app");
