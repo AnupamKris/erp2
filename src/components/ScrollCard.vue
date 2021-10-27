e339052788291ec7d54e423889d4576f1be15738
DGGowtham <80588482+DGGowtham@users.noreply.github.com>
2021-10-27 12:29:01 +0530

    Added: many things

---
+++ src/components/ScrollCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="cards">
+    <div className="card" v-for="card in cards" :key="card.id">
+      {{ card.id }} -- {{ card.data }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const currentCard = ref(0);
+    const cards = ref([
+      {
+        id: 1,
+        data: "card1",
+      },
+    ]);
+    return {
+      cards,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.card {
+  width: 300px;
+  height: 300px;
+  background: #131313;
+</style>
+4141#4141$: dfnuorgkcabn;
+</style>
